08bf815a8526c913eda960c562f231c89a5be3ac
Nicky Liu <nickyliu91@gmail.com>
2018-08-12 10:01:32 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,8 @@ def prime?(number)
   i = 2
   return true if number.abs == 1
   return false if number * -1 == -number
-  while i < number.abs
-    return false if number.abs % i == 0
+  while i < number
+    return false if number % i == 0
     i += 1
   end
   true
